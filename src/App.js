9399eae85bb84221cdf51f7d8365f6bcf36c33cc
lennoxstark47 <twisamhaldar@gmail.com>
2021-04-26 11:12:42 +0530

    finalized

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
 	const handleCalculation = () => {
 		setAnswer(eval(expression));
-		setExpression((prev) => prev + '=');
+		// setExpression((prev) => prev + '=');
 	};
 
 	const handleAllClear = () => {
@@ -24,7 +24,7 @@ const App = () => {
 			<div className='container'>
 				<div className='wrapper'>
 					<div className='grid'>
-						<div className='display'>
+						<div className='display' id='display'>
 							<input
 								type='text'
 								value={expression}
@@ -36,72 +36,86 @@ const App = () => {
 						</div>
 						<div
 							onClick={handleAllClear}
-							className='pad-button AC'>
+							className='pad-button AC'
+							id='clear'>
 							AC
 						</div>
 						<div
 							onClick={() => handleClick('/')}
-							className='pad-button division operator'>
+							className='pad-button division operator'
+							id='divide'>
 							/
 						</div>
 						<div
 							onClick={() => handleClick('*')}
-							className='pad-button multiplication operator'>
+							className='pad-button multiplication operator'
+							id='multiply'>
 							x
 						</div>
 						<div
 							onClick={() => handleClick('7')}
-							className='pad-button seven'>
+							className='pad-button seven'
+							id='seven'>
 							7
 						</div>
 						<div
 							onClick={() => handleClick('8')}
-							className='pad-button eight'>
+							className='pad-button eight'
+							id='eight'>
 							8
 						</div>
 						<div
 							onClick={() => handleClick('9')}
-							className='pad-button nine'>
+							className='pad-button nine'
+							id='nine'>
 							9
 						</div>
 						<div
 							onClick={() => handleClick('-')}
-							className='pad-button minus operator'>
+							className='pad-button minus operator'
+							id='subtract'>
 							-
 						</div>
 						<div
 							onClick={() => handleClick('4')}
-							className='pad-button four'>
+							className='pad-button four'
+							id='four'>
 							4
 						</div>
 						<div
 							onClick={() => handleClick('5')}
-							className='pad-button five'>
+							className='pad-button five'
+							id='five'>
 							5
 						</div>
 						<div
 							onClick={() => handleClick('6')}
-							className='pad-button six'>
+							className='pad-button six'
+							id='six'>
 							6
 						</div>
 						<div
 							onClick={() => handleClick('+')}
-							className='pad-button addition operator'>
+							className='pad-button addition operator'
+							id='add'>
 							+
 						</div>
 						<div
 							onClick={() => handleClick('1')}
-							className='pad-button one'>
+							className='pad-button one'
+							id='one'>
 							1
 						</div>
 						<div
 							onClick={() => handleClick('2')}
-							className='pad-button two'>
+							className='pad-button two'
+							id='two'>
 							2
 						</div>
 						<div
 							onClick={() => handleClick('3')}
-							className='pad-button three'>
+							className='pad-button three'
+							id='three'>
 							3
 						</div>
 						<div
@@ -113,17 +127,27 @@ const App = () => {
 						</div>
 						<div
 							onClick={() => handleClick('0')}
-							className='pad-button zero'>
+							className='pad-button zero'
+							id='zero'>
 							0
 						</div>
 						<div
 							onClick={() => handleClick('.')}
-							className='pad-button dot'>
+							className='pad-button dot'
+							id='decimal'>
 							.
 						</div>
 					</div>
 				</div>
 			</div>
+			<footer
+				style={{
+					textAlign: 'center',
+					fontFamily: 'Share Tech Mono',
+					color: 'rgb(172, 57, 57)',
+				}}>
+				by twisam
+			</footer>
 		</div>
 	);
 };
